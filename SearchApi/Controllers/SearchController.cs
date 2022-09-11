7fa13f4ef8bd69ae5e64850d253bc1cb79168ac6
Masuma Khanom <masuma.khanom098@gmail.com>
2022-09-11 19:10:10 +0200

    Update SearchController

--- SearchApi/Controllers/SearchController.cs
+++ SearchApi/Controllers/SearchController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SearchCore;
 using SearchCore.Model;
+using System.Reflection;
 
 namespace SearchApi.Controllers
 {
@@ -19,17 +22,28 @@ namespace SearchApi.Controllers
         }
 
         [HttpGet(Name = "GetSearchResults")]
-        public IEnumerable<SearchableEntity> Get()
+        public async Task<IEnumerable<SearchableEntity>> Get(string searchKey)
         {
-            var dunmmies = new List<SearchableEntity>();
-            var b = new Building();
-            b.Name = "Test1";
+            var data = await GetDataAsync();
+            if(data == null) return new List<SearchableEntity>();
 
-            var l = new SmartLock();
-            l.Name = "Test2";
-            dunmmies.Add(b);
-            dunmmies.Add(l);
-            return dunmmies;
+            var search = new Search(data);
+            var result = search.GetResults(searchKey);
+            return result;
+            
+        }
+
+        private async Task<SearchDataSet> GetDataAsync()
+        {
+            SearchDataSet? data = null;
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(@"https://simonsvoss-homework.herokuapp.com/sv_lsm_data.json");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                data = JsonConvert.DeserializeObject<SearchDataSet>(jsonString);
+            }
+            return data;
         }
     }
 }
